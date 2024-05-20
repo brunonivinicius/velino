8880bce9d6d5e5361d8b78537f77591e33fa3ee7
Vinicius <125091227+brunonivinicius@users.noreply.github.com>
2024-05-20 11:37:53 -0300

    funcionando1

--- src/DocumentNFT.sol
+++ src/DocumentNFT.sol
@@ -1,36 +1,56 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DocumentNFT is ERC721, Ownable {
-    uint256 private _tokenIdCounter;
-    mapping(uint256 => string) private _documentHashes;
+contract DocumentNFT is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+    mapping(uint256 => bool) private _locked;
 
-    constructor() ERC721("DocumentNFT", "DOCNFT") {}
+    constructor(
+        address initialOwner
+    ) ERC721("DocumentNFT", "DOCNFT") Ownable(initialOwner) {}
 
-    function mintDocumentNFT(string memory documentHash) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter;
-        _tokenIdCounter += 1;
+    event Locked(uint256 tokenId);
 
-        _mint(msg.sender, tokenId);
-        _documentHashes[tokenId] = documentHash;
+    function locked(uint256 tokenId) external view returns (bool) {
+        require(ownerOf(tokenId) != address(0));
+        return _locked[tokenId];
     }
 
-    function getDocumentHash(
-        uint256 tokenId
-    ) public view returns (string memory) {
-        require(_exists(tokenId), "Token does not exist");
-        return _documentHashes[tokenId];
+    function safeMint(string memory hash) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _locked[tokenId] = true;
+        emit Locked(tokenId);
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, hash);
     }
 
-    function _beforeTokenTransfer(
-        address from,
+    function _update(
         address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721) returns (address) {
+        address from = _ownerOf(tokenId);
+        if (from != address(0)) {
+            revert("Transfer not allowed"); // Prevent all transfers, making the token soulbound and non-burnable
+        }
+        return super._update(to, tokenId, auth);
+    }
+
+    // Required overrides
+
+    function tokenURI(
         uint256 tokenId
-    ) internal override {
-        require(from == address(0), "This token is non-transferable");
-        super._beforeTokenTransfer(from, to, tokenId);
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
--- test/DocumentNFT.t.sol
+++ test/DocumentNFT.t.sol
@@ -1,20 +1,62 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/DocumentNFT.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {DocumentNFT} from "../src/DocumentNFT.sol";
 
 contract DocumentNFTTest is Test {
-    DocumentNFT private nft;
+    DocumentNFT docnft;
+    address owner = address(1);
+    address user = address(2);
 
     function setUp() public {
-        nft = new DocumentNFT();
+        // Deploy the DocumentNFT contract
+        docnft = new DocumentNFT(owner);
+        // Transfer ownership to the designated owner address
+        docnft.transferOwnership(owner);
     }
 
-    function testMinting() public {
-        nft.mintDocumentNFT("hash-do-documento");
+    function testSafeMint() public {
+        vm.startPrank(owner);
+        string memory uri = "https://example.com/token/1";
+        docnft.safeMint(uri);
+        vm.stopPrank();
 
-        assertEq(nft.ownerOf(0), address(this));
-        assertEq(nft.getDocumentHash(0), "hash-do-documento");
+        // Check the balance of the owner (should be 1)
+        assertEq(
+            docnft.balanceOf(owner),
+            1,
+            "Balance should be 1 after minting"
+        );
+        // Check the owner of the minted token (should be the contract owner)
+        assertEq(docnft.ownerOf(0), owner, "Owner should own the minted token");
+        // Check the token URI
+        assertEq(
+            docnft.tokenURI(0),
+            uri,
+            "Token URI should match the provided URI"
+        );
+    }
+
+    function testLockingAfterMint() public {
+        vm.startPrank(owner);
+        string memory uri = "https://example.com/token/1";
+        docnft.safeMint(uri);
+        vm.stopPrank();
+
+        // Check if the token is locked
+        bool isLocked = docnft.locked(0);
+        assertTrue(isLocked, "Token should be locked after minting");
+    }
+
+    function testTransferNotAllowed() public {
+        vm.startPrank(owner);
+        string memory uri = "https://example.com/token/1";
+        docnft.safeMint(uri);
+        vm.stopPrank();
+
+        // Attempt to transfer the token and expect it to revert
+        vm.expectRevert("Transfer not allowed");
+        docnft.safeTransferFrom(owner, user, 0);
     }
 }
